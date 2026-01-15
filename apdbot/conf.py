76372ba4228319aee307f517ca4d602578233f9a
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-15 13:44:10 +0530

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,9 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
-    APP_ID = 22299340
+    BOT_TOKEN = "YOUR_BOT_TOKEN_HERE" # Make sure to add your token here!
+    API_ID = 22299340                # Changed from APP_ID to API_ID
     API_HASH = "09b09f3e2ff1306da4a19888f614d937"
-    OWNER_ID = "5380609667"
+    OWNER_ID = 5380609667            # Recommended: use integer without quotes
     AUTH = [5380609667, -1003533392768]
+    
